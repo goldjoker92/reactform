17fd34b2c7b69f72d444bd37a77c7e853943d1e3
unknown <guillaumeragot92@yahoo.com>
2021-03-09 22:48:53 +0100

    second commit

---
+++ src/Form.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormSignin from './FormSignin'
+
+const Form = () => {
+    return (
+        <div>
+            <FormSignin />
+        </div>
+    )
+}
+
+export default Form
